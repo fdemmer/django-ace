e365c8cf2cb40887da3f4c7c2c88f91b7ab7dc6f
Florian Demmer <fdemmer@gmail.com>
2020-04-26 17:20:07 +0200

    Add TabularInline to example

--- example/example/app/admin.py
+++ example/example/app/admin.py
@@ -3,11 +3,25 @@ from django.db import models
 
 from django_ace import AceWidget
 
-from .models import Snippet
+from .models import Snippet, SnippetGroup
+
+default_ace_widget = AceWidget(mode='html', theme='textmate')
 
 
 @admin.register(Snippet)
 class SnippetAdmin(admin.ModelAdmin):
     formfield_overrides = {
-        models.TextField: {'widget': AceWidget(mode='html', theme='textmate')},
+        models.TextField: {'widget': default_ace_widget},
     }
+
+
+class SnippetInline(admin.TabularInline):
+    model = Snippet
+    formfield_overrides = {
+        models.TextField: {'widget': default_ace_widget},
+    }
+
+
+@admin.register(SnippetGroup)
+class SnippetGroupAdmin(admin.ModelAdmin):
+    inlines = [SnippetInline]
