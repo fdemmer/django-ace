960432d6dbd530a8061a7eba1b7f08d31826309d
Florian Demmer <fdemmer@gmail.com>
2020-04-26 14:52:16 +0200

    Set upcoming version and require Python3

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
 
 setup(
     name="django-ace",
-    version="1.1.0rc1",
+    version="2.0.0b",
     description="django-ace provides integration for ajax.org ACE with Django",
     long_description=open("README.rst").read(),
     author="Bradley Ayers",
@@ -12,15 +11,15 @@ setup(
     url="https://github.com/django-ace/django-ace",
     packages=find_packages(exclude=["example", "example.*"]),
     include_package_data=True,
-    install_requires=["Django>1.11"],
+    install_requires=["Django>=2.2"],
     tests_require=["html5lib"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Framework :: Django",
     ],
+    python_requires='>=3.6',
 )
