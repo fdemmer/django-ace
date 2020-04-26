795668f7c5d3a14e22ebdc3500f0c780fe50048b
Florian Demmer <fdemmer@gmail.com>
2020-04-26 17:20:07 +0200

    Add TabularInline to example

--- example/example/app/models.py
+++ example/example/app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 
+class SnippetGroup(models.Model):
+    name = models.CharField(max_length=64)
+
+
 class Snippet(models.Model):
+    group = models.ForeignKey(
+        SnippetGroup,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+    )
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
